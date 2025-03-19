d9b25c7a70cf2aefa1cd9bccd1b00fe186b62ac6
Raymond Lam <raymond.lam122@gmail.com>
2025-03-19 01:48:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello from CI/CD Pipeline!");
 });
 
+
+app.get("/api", (req, res) => {
+  res.send("Hello from api route!");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
